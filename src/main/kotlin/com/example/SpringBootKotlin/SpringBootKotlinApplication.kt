54d9d16509741742d20a67c95ad1a3e66528f9cb
nfernan1 <n.fernando65@gmail.com>
2018-05-15 21:42:35 -0700

    Updating main with controller

--- src/main/kotlin/com/example/SpringBootKotlin/SpringBootKotlinApplication.kt
+++ src/main/kotlin/com/example/SpringBootKotlin/SpringBootKotlinApplication.kt
@@ -1,11 +1,16 @@
 package com.example.SpringBootKotlin
 
+import controller.AppController
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class SpringBootKotlinApplication
-
+class SpringBootKotlinApplication {
+    @Bean
+    fun controller() = AppController()
+}
 fun main(args: Array<String>) {
     runApplication<SpringBootKotlinApplication>(*args)
 }
+
